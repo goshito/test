2d86c1bbc4cb0b6776103eab7f1d042ab9885d22
Georgi Toshev <georgi.toshev@gmail.com>
2016-01-27 16:40:54 +0200

    char functions

--- main.c
+++ main.c
@@ -1,52 +1,61 @@
 #include <stdio.h>
+#include <ctype.h>
 #include <string.h>
 
-#define MAXSTRLEN 200
+char mystring[] = "What the fuck is this shit 0x00???";
 
-char mystring[] = "On the 2nd day of Christmas my true love gave to me, 2 turtle doves and a partridge in a pear tree.";
-char tens[] =     "         10        20        30        40        50";
-char nums[] =     "123456789012345678901234567890123456789012345678901234567890";
-char msg1[MAXSTRLEN] = "Result1: ";
-char msg2[MAXSTRLEN] = "Result2: ";
-char myotherstr[6];
-
-void showstring() {
-    printf("\n\n%s\n", mystring);
-    printf("%s\n%s\n", tens, nums);
-}
-
-void searchstring(char searchstr[]) {
-    char *ptrtostr;
-    int foundat;
-    ptrtostr = strstr(mystring, searchstr);
-    foundat = (int)((ptrtostr - mystring) + 1); //The subtraction of the two strins decimal values gives the position - 1
-    if (ptrtostr != 0)
-        printf("\n'%s' found at position %d\n", searchstr, foundat);
-    else 
-        printf("\n'%s' not found \n", searchstr);    
-}
-
-void copystring() {
-    myotherstr[0] = 0;
-    strncpy(myotherstr, "Easter", 4);
-    printf("\nCopied this string: '%s'", myotherstr);
-}
-
-void concatstring() {
-    strcat(msg1, "Easter");
-    printf("\n\nstrcat: '%s'", msg1);
-    strncat(msg2, "Easter", 4);
-    printf("\nstrcat: '%s'\n", msg2);
+void chartypes() {
+    int i;
+    char c;
+    int numDigits = 0;
+    int numLetters = 0;
+    int numUpCase = 0;
+    int numLowCase = 0;
+    int numSpaces = 0;
+    int numPunct = 0;
+    int numCtl = 0;
+    int numUnknown = 0;
+    int lenghtOfStr = strlen(mystring);
+    
+    for (i = 0; i < lenghtOfStr; i++) {
+        c = mystring[i];
+        if (isalpha(c)) {
+            numLetters++;
+            if (isupper(c)) {
+                printf("'%c' [uppercase character]\n", c);
+                numUpCase++;
+            } else {
+                printf("%c [lowecase character]\n", c);
+                numLowCase++;
+            }
+        } else if (isdigit(c)) {
+            printf("'%c' [digit]\n", c);
+            numDigits++;
+        } else if (ispunct(c)) {
+            printf("'%c' [puntuation]\n", c);
+            numPunct++;
+        } else if (isblank(c)) {
+            printf("'%c' [blank]\n", c);
+            numSpaces++;
+        } else if (iscntrl(c)) {
+            printf("'%c' [ctrl]\n", c);
+            numCtl++;
+        } else {
+        printf("'%c' [unknown]\n", c);
+        numUnknown++;
+       }
+    }
+    printf("The string contains %d characters: %d letters (%d uppercase, %d lowercase)\n", lenghtOfStr, numLetters, numUpCase, numLowCase);
+    printf( "%d digits, %d ctrl, %d punctuation characters, %d spaces and %d unclassified characters.\n", numDigits, numCtl, numPunct, numSpaces, numUnknown);
 }
 
 
 int main() {
-    printf("The lenght of mystring = %d", strlen(mystring));
-    printf("\nThe lenght of 'Hello' = %d", strlen("Hello"));
-    showstring();
-    searchstring("2nd");
-    searchstring("Christmas");
-    searchstring("Easter");
-    copystring();
-    concatstring();
+    if (isalpha('x')) {
+        printf("'x' is a letter");
+    } else {
+        printf("'x' is not a letter\n");
+    }
+    chartypes();
+    return 0;
 }
