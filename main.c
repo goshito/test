4206a7f9afede8b3aec6fb525d05481f0d416946
Georgi Toshev <georgi.toshev@gmail.com>
2016-01-28 16:34:22 +0200

    enum with cd records database

--- main.c
+++ main.c
@@ -1,47 +1,59 @@
 #include <stdio.h>
+#include <string.h>
 
-enum days {
-    Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
+#define NUMBER_OF_CDS 4
+
+enum score {
+    Terrible, Bad, Average, Good, Excellent, Ebasi_yakoto, Nema_takiva_hitove, Klasika_bate, Shitnya
 };
 
-enum suits {
-    Hearts, Diamonds, Clubs, Spades
-};
+typedef char Str50[50]; 
 
-enum numbers {
-    Couple = 2, Dozen = 12, Score = 20
-};
+typedef struct cd {
+    Str50 name;
+    Str50 artist;
+    int trackcount;
+    enum score rating;
+} CD;
 
-void showdaytype(enum days day) {
-    if ((day == Saturday) || (day == Sunday)) {
-        printf("It's weekend!\n");
-    } else {
-        printf("It's a working day.\n");
-    }
+//struct cd cd_collection[NUMBER_OF_CDS];
+CD cd_collection[NUMBER_OF_CDS];
+
+void create_cdcollection() {
+    strcpy(cd_collection[0].name, "Ebasi hitovete");
+    strcpy(cd_collection[0].artist, "Briana Boobs");
+    cd_collection[0].trackcount = 20;
+    cd_collection[0].rating = Good;     
+    
+    strcpy(cd_collection[1].name, "Bash hitovete");
+    strcpy(cd_collection[1].artist, "Gode&Bilche Beats");
+    cd_collection[1].trackcount = 3;
+    cd_collection[1].rating = Nema_takiva_hitove;
+    
+    strcpy(cd_collection[2].name, "Retro chalga");
+    strcpy(cd_collection[2].artist, "Marko i Snejina");
+    cd_collection[2].trackcount = 8;
+    cd_collection[2].rating = Klasika_bate;
+    
+    strcpy(cd_collection[3].name, "Drisko Beats");
+    strcpy(cd_collection[3].artist, "Drisko");
+    cd_collection[3].trackcount = 9;
+    cd_collection[3].rating = Shitnya;
 }
 
-void showcard (enum suits card) {
-    if ((card == Hearts) || (card == Diamonds)) {
-    //if ((card == 0) || (card == 1)) {
-        printf("This card is red (in suit %d).\n", card);
-    } else {
-        printf("This is a black card.\n");
-    }
-}
 
-void buyinbulk(enum numbers num) {
-    printf("The customer wants to buy %d items.\n", num);
+void display_cdcollection() {
+    int i;
+    //struct cd thiscd;
+    CD thiscd;
+    for (i = 0; i < NUMBER_OF_CDS; i++) {
+        thiscd = cd_collection[i];
+        printf("CD #%d: '%s' by %s has %d tracks. My rating = %d\n", i, thiscd.name, thiscd.artist, thiscd.trackcount, thiscd.rating);
+    }
 }
 
 int main() {
-    enum days today;
-    enum suits playingcard;
-    enum numbers num;
-    today = Saturday;
-    playingcard = Diamonds;
-    num = Dozen;
-    showdaytype(today);
-    buyinbulk(num);
-    showcard(playingcard);
+    create_cdcollection();
+    display_cdcollection();
     return 0;
 }
