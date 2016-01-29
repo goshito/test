1d33fabe460d5c0a2f61c8410f10d950089612a8
Georgi Toshev <georgi.toshev@gmail.com>
2016-01-30 00:39:41 +0200

    uncesored file operations

--- main.c
+++ main.c
@@ -1,47 +1,40 @@
 #include <stdio.h>
 
-#define MAXSTRLEN 200
-#define FILENAME "test.txt"
+#define MAXSTRLEN 30
+#define FILENAME "cveta.txt"
+#define EMPTYFILENAME "nema.txt"
+#define INVALIDFILENAME "sex.txt"
 
-void readlines() {
-    FILE *fp = fopen(FILENAME, "r");
+void linecount(char *fn) {
+    int numlines = 0;
     char line[MAXSTRLEN];
+    FILE *fp = fopen(fn, "r");
     if (fp != 0) {
-        while ( fgets (line, sizeof(line), fp) != 0 ) {
-            fputs(line, stdout);
+        while (fgets(line, sizeof(line), fp) != 0) {
+            numlines++;
         }
         fclose(fp);
+        printf("%s contains %d lines of text.\n", fn, numlines);
     } else {
-        printf("File %s cannot be opened", FILENAME);
-    }
-}
-
-void writelines() {
-    FILE *fp;
-    fp = fopen(FILENAME, "w");
-    fputs("Hello world\n", fp);
-    fputs("Goodbye Mars\n", fp);
-    fclose(fp);
-}
-
-void clearfile() {
-    FILE *fp;
-    fp = fopen(FILENAME, "w");
-    fclose(fp);
-}
-
-void deletefile() {
-    if (remove(FILENAME) == 0) {
-        printf("%s file deleted.\n", FILENAME);
-    } else {
-        printf("Unable to delete the file: %s.\n", FILENAME);
+        printf("file %s cannot be opened", fn);
     }
 }
 
 int main() {
-    //writelines();
-    //readlines();
-    //deletefile();
-    clearfile();
+    FILE *fp;
+    fp = fopen("cveta.txt", "w");
+    fputs("I want put my tongue into Cveta's mouth...\n", fp);
+    fputs("...because she is asking for it.", fp);
+    fclose(fp);
+    
+    fp = fopen("cveta.txt", "a");
+    fputs("I also want to fuck her", fp);
+    fclose(fp);
+    
+    fp = fopen("nema.txt", "w");
+    fclose(fp);
+    linecount(FILENAME);
+    linecount(EMPTYFILENAME);
+    linecount(INVALIDFILENAME);
     return 0;
 }
