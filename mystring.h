fe25e1d4c0c72e8ede5fd619f6b9ddf92b01a6ff
Georgi Toshev <georgi.toshev@gmail.com>
2016-01-29 18:42:20 +0200

    multifile project

---
+++ mystring.h
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   mystring.h
+ * Author: georgi
+ *
+ * Created on January 28, 2016, 8:25 PM
+ */
+
+#ifndef MYSTRING_H
+#define MYSTRING_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* MYSTRING_H */
+
+#define BUFSIZE 100
+int readln(char[], int);
+int searchstring(char[], char[]);
+
