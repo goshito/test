69b36feeebbea7ea75e4ed1844285d847a35e6af
Georgi Toshev <georgi.toshev@gmail.com>
2016-01-29 23:46:34 +0200

    reading and writing text file

--- main.c
+++ main.c
@@ -1,21 +1,47 @@
 #include <stdio.h>
-#include "mystring.h"
 
-char b[BUFSIZE];
-char teststring[] = "Hello world";
+#define MAXSTRLEN 200
+#define FILENAME "test.txt"
+
+void readlines() {
+    FILE *fp = fopen(FILENAME, "r");
+    char line[MAXSTRLEN];
+    if (fp != 0) {
+        while ( fgets (line, sizeof(line), fp) != 0 ) {
+            fputs(line, stdout);
+        }
+        fclose(fp);
+    } else {
+        printf("File %s cannot be opened", FILENAME);
+    }
+}
+
+void writelines() {
+    FILE *fp;
+    fp = fopen(FILENAME, "w");
+    fputs("Hello world\n", fp);
+    fputs("Goodbye Mars\n", fp);
+    fclose(fp);
+}
+
+void clearfile() {
+    FILE *fp;
+    fp = fopen(FILENAME, "w");
+    fclose(fp);
+}
+
+void deletefile() {
+    if (remove(FILENAME) == 0) {
+        printf("%s file deleted.\n", FILENAME);
+    } else {
+        printf("Unable to delete the file: %s.\n", FILENAME);
+    }
+}
 
 int main() {
-    int i;
-    int stringpos;
-    
-    printf("starting...\n");
-    i = readln(b, sizeof(b));
-    printf("b=%s; i=%d\n", b, i);
-    stringpos = searchstring(b, teststring);
-    if (stringpos > -1) {
-        printf("'%s' found in '%s' at index #%d\n", b, teststring, stringpos);
-    } else {
-        printf("'%s' not found in '%s'\n", b, teststring);
-    }
+    //writelines();
+    //readlines();
+    //deletefile();
+    clearfile();
     return 0;
 }
